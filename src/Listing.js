6355de12892f5de1ea2b7c682e823a348044752e
Austin Wolfe <wolfe_austin@yahoo.com>
2018-05-08 16:26:20 -0400

    CRUD functionality FrontEnd

---
+++ src/Listing.js
@@ -0,0 +1,10 @@
+class Listing{
+  constructor(id, host_id, name, picture, description, location){
+    this.id = id
+    this.host_id = host_id
+    this.name = name
+    this.picture = picture
+    this.description = description
+    this.location = location
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
+const baseUrl = "http://localhost:3000/api/v1/"
+const listingsUrl = `${baseUrl}listings`
+const reservationsUrl = `${baseUrl}reservations`
+const usersUrl = `${baseUrl}users`
+
+let currentListings
+
+let listingUl = document.getElementById('listings')
+
 document.addEventListener('DOMContentLoaded', () => {
-  alert('LOADED');
+  fetch(listingsUrl)
+    .then(response => response.json())
+    .then(json => renderHomepage(json))
+
+  listingUl.addEventListener('click', function(e){
+    //we need to render listing page with id of e.target.dataset.id
+    console.log(e.target.dataset.id)
+    fetch(`${listingsUrl}/${e.target.dataset.id}`)
+      .then(response => response.json())
+      .then(json => renderListing(json))
+  })
+
 });
+
+function renderListing(json){
+  //render HTML to show single Listing info
+  //remove UL elements
+  listingUl.outerHTML = ""
+
+  //add elements needed for Listing(show)
+  var temp = document.createElement('h3')
+  temp.innerHTML = `Listing Name: ${json.name} Picture: ${json.picture} Description: ${json.description}`
+  document.body.appendChild(temp)
+  //add new
+  console.log(json, 'empty ul?')
+
+
+}
+
+function renderHomepage(json){
+  createListings(json)
+  //render any other elements that will be on the homepage
+}
+
+function createListings(arrayOfListings){
+
+  arrayOfListings.forEach(element => {
+    //create listing object
+    var temp = document.createElement('li')
+    temp.innerHTML = `Name: ${element.name} Photo: ${element.picture} Description: ${element.description}`
+    let newListing = new Listing(element.id, element.host_id, element.name, element.picture, element.description, element.location)
+    temp.setAttribute('data-id', newListing.id)
+    listingUl.appendChild(temp)
+  })
+
+}
